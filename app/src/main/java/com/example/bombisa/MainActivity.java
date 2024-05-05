ac4f79df4033bc8e24f99366cdcb27ef289efc32
Ainz Sama <145613771+lordlance1@users.noreply.github.com>
2024-05-05 13:45:28 -0300

    feat: agregar funcion de contador

--- app/src/main/java/com/example/bombisa/MainActivity.java
+++ app/src/main/java/com/example/bombisa/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.bombisa;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int contador = 0;
+    private TextView contadorTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +24,17 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Button botonSumar = findViewById(R.id.pedidos);
+        contadorTextView = findViewById(R.id.contadorTextView);
+
+        botonSumar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                contador++;
+
+                contadorTextView.setText(String.valueOf("Panes pedidos: " +contador ));
+            }
+        });
     }
-}
+    }

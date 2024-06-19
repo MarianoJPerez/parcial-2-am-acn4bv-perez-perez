9bba2bf088bb1508f5062f842966c3ac29241461
MarianoJPerez <marianperez77@outlook.com>
2024-06-19 18:00:01 -0300

    Feat: boton para salir

--- app/src/main/java/com/example/bombisa/MainActivity.java
+++ app/src/main/java/com/example/bombisa/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.bombisa;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +11,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.bombisa.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
     private int contador = 0;
@@ -36,5 +39,19 @@ public class MainActivity extends AppCompatActivity {
                 contadorTextView.setText(String.valueOf("Panes pedidos: " +contador ));
             }
         });
+        Button botonCerrarSesion = findViewById(R.id.button_logout);
+        botonCerrarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cerrarSesion();
+            }
+        });
     }
-    }
+
+
+    private void cerrarSesion() {
+        Intent intent = new Intent(MainActivity.this, Login.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }}
